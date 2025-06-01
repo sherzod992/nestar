0d97043b7fcf99239262316ea7347a823c53165b
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-06-01 15:19:22 +0900

    feat:build project standarts

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,6 +5,9 @@ import { ConfigModule } from '@nestjs/config';
 import {GraphQLModule} from '@nestjs/graphql';
 import{ ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
+
 
 @Module({
   imports: [ConfigModule.forRoot(),
@@ -13,7 +16,9 @@ import { AppResolver } from './app.resolver';
       playground: true,
       uploads:false,
       autoSchemaFile: true,
-    })
+    }),
+    ComponentsModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
