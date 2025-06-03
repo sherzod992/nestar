4249ab826bf2ce7bc0fc230eceb79dd44c990fd5
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-06-03 22:27:11 +0900

    feat: create Schema Models and project enum

---
+++ apps/nestar-api/src/schemas/Member.model.ts
@@ -0,0 +1,87 @@
+import { Schema } from "mongoose";
+import { MemberAuthType, MemberStatus, MemberType, } from "../libs/enums/member.enum"; 
+const MemberSchema = new Schema({
+    memberType:{
+        type: String,
+        enum:MemberType,
+        default: MemberType.USER,
+    },
+    memberStatus:{
+        type: String,
+        enum: MemberStatus,
+        default: MemberStatus.ACTIVE,
+    },
+    memberAuthType:{
+        type: String,
+        enum: MemberAuthType,
+        default: MemberAuthType.PHONE,
+    },
+    memberPhone:{
+        type: String,
+        index:{ unique: true, sparse: true },
+        require : true,
+    },
+    memberNick:{
+        type:String,
+        index:{ unique: true, sparse: true },
+        require: true,
+    },
+    memberPassword:{
+        type: String,
+        select: false,
+        require: true,
+    },
+    memberFullName:{
+        type: String,
+    },
+    memberImage:{
+        type: String,
+        default:' ',
+    },
+    memberAdress:{
+        type: String,
+    },
+    memberDesc:{
+        type: String,
+    },
+    memberProperties:{
+        type: Number,
+        default: 0,
+    },
+    memberArticles:{
+        type: Number,
+        default: 0,
+    },
+    memberFollowers:{
+        type: Number,
+        default: 0,
+    },
+    memberLikes:{
+        type: Number,
+        default: 0,
+    },
+    memberComments:{
+        type: Number,
+        default: 0,
+    },
+    memberRank:{
+        type: Number,
+        default: 0,
+    },
+    memberWarnings:{
+        type: Number,
+        default: 0,
+    },
+    memberBlocks:{
+        type: Number,
+        default: 0,
+    },
+   
+
+
+    },
+    {timestamps: true, collection: 'members'},
+);
+
+
+export default MemberSchema;
