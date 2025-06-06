576086903e940acbe99f61ef918f3363bd3b3da5
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-06-06 19:00:08 +0900

    ? qoyildi

--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -31,7 +31,7 @@ export class Member{
     @Field(()=>String,{nullable:true})
     memberImage:string;
     @Field(()=>String,{nullable:true})
-    memberAdress:string;
+    memberAdress?:string;
 
     @Field(()=>String,{nullable:true})
     memberDesc?:string;
