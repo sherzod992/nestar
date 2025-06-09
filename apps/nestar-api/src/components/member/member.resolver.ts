80c00146e234e9d64158fa1c85b8597bbe075a4b
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-06-09 14:49:41 +0900

    fix:global pipe validation

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
 
@@ -14,7 +14,6 @@ export class MemberResolver {
     constructor(private readonly memberService:MemberService){}
     
     @Mutation(()=>Member)
-    @UsePipes(ValidationPipe)
     public async signup(@Args('input')input:MemberInput):Promise<Member>{
         try{
             console.log('Mutation: signup');
@@ -28,7 +27,6 @@ export class MemberResolver {
     }
 
     @Mutation(()=>Member)
-    @UsePipes(ValidationPipe)
     public async login(@Args('input')input:LoginInput):Promise<Member>{
         try{
             console.log('Mutation: login');
--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,9 +1,11 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT_API || 3000);
   console.log(`🚀 API running on port ${process.env.PORT_API}`);
 }
