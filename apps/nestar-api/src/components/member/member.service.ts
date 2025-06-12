4d5666351e4edc82aaa7548640361137b2233267
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-06-12 23:48:38 +0900

    fix:modify view mdule to records

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -8,11 +8,16 @@ import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { T } from '../../libs/types/common';
+import { ViewService } from '../view/view.service';
+import { ViewGroup } from '../../libs/enums/view.enum';
 @Injectable()
 export class MemberService {
-
-    constructor(@InjectModel("Member")private readonly memberModel:Model<Member>, 
-    private authService:AuthService){}
+    
+  constructor(
+    @InjectModel("Member") private readonly memberModel: Model<Member>, 
+    private authService: AuthService,
+    private viewService: ViewService, 
+  ) {}
 
     public async signup(input:MemberInput): Promise<Member> {
       input.memberPassword= await this.authService.hashPassword(input.memberPassword)
@@ -75,6 +80,16 @@ export class MemberService {
       };
       const targetMember = await this.memberModel.findOne(search).lean().exec();
       if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+      if (memberId) {
+        // record view
+        const viewInput = { memberId: memberId, viewRefId: targetId, viewGroup: ViewGroup.MEMBER };
+        const newView = await this.viewService.recordView(viewInput);
+        // increase memberView
+        if (newView) {
+          await this.memberModel.findOneAndUpdate(search, { $inc: { memberViews: 1 } }, { new: true }).exec();
+          targetMember.memberViews++;
+        }
+      }
       return targetMember;
     }
     
