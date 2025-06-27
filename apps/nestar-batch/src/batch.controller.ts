a48f1437056de95cdddaf6390591006cd03bebfc
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-06-27 21:16:18 +0900

    joyladim

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,18 +1,25 @@
 import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
-import { Cron, Interval, Timeout } from "@nestjs/schedule";
-import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from "./lib/config";
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import {
+  BATCH_ROLLBACK,
+  BATCH_TOP_AGENTS,
+  BATCH_TOP_PROPERTIES
+} from './lib/config';
 
 @Controller()
 export class BatchController {
-	private logger: Logger = new Logger('BatchController');
-	constructor(private readonly BatchService: BatchService) {}
+  private logger: Logger = new Logger('BatchController');
 
-	@Timeout(1000)
-	handleTimeout() {
-		this.logger.debug('BATCH SERVER READY');
-	}
-	@Cron('00 ***', { name: BATCH_ROLLBACK })
+  constructor(private readonly BatchService: BatchService) {}
+
+  @Timeout(1000)
+  handleTimeout() {
+    this.logger.debug('BATCH SERVER READY');
+  }
+
+  // Har kuni soat 00:00 da ishga tushadi
+  @Cron('0 0 * * *', { name: BATCH_ROLLBACK })
   public async batchRolBack() {
     try {
       this.logger.debug['context'] = BATCH_ROLLBACK;
@@ -21,38 +28,40 @@ export class BatchController {
     } catch (err) {
       this.logger.error(err);
     }
-	}
+  }
 
-	@Cron('20 ***', { name: BATCH_TOP_PROPERTIES })
+  // Har kuni soat 00:20 da ishga tushadi
+  @Cron('20 0 * * *', { name: BATCH_TOP_PROPERTIES })
   public async batchProperties() {
     try {
-    this.logger.debug['context'] = BATCH_TOP_PROPERTIES;
+      this.logger.debug['context'] = BATCH_TOP_PROPERTIES;
       this.logger.debug('EXECUTED');
-       await this.BatchService.batchTopProperties();
-			} catch (err) {
-				this.logger.error(err);
-			}
-		
-	}
+      await this.BatchService.batchTopProperties();
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
 
-	@Cron('40 ***', { name: BATCH_TOP_AGENTS })
+  // Har kuni soat 00:40 da ishga tushadi
+  @Cron('40 0 * * *', { name: BATCH_TOP_AGENTS })
   public async batchAgents() {
     try {
-     this.logger.debug['context'] = BATCH_TOP_AGENTS;
+      this.logger.debug['context'] = BATCH_TOP_AGENTS;
       this.logger.debug('EXECUTED');
       await this.BatchService.batchTopAgents();
-			} catch (err) {
-				this.logger.error(err);
-			}
-	}
+    } catch (err) {
+      this.logger.error(err);
+    }
+  }
 
-	@Interval(1000)
-	handleInterval() {
-		this.logger.debug('INTERNAL TEST');
-	}
+  // Har 100 soniyada bir marta ishlaydi (log yozish uchun test)
+  @Interval(100000)
+  handleInterval() {
+    this.logger.debug('INTERNAL TEST');
+  }
 
-	@Get()
-	getHello(): string {
-		return this.BatchService.getHello();
-	}
+  @Get()
+  getHello(): string {
+    return this.BatchService.getHello();
+  }
 }
